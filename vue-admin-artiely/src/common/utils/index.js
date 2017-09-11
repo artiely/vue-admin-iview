175f54337cd886d08998ec86e8a7d740032a7f2f
artiely <artiely1220@gmail.com>
2017-09-11 09:23:51 +0800

    修复单页应用无法获取参数bug

--- vue-admin-artiely/src/common/utils/index.js
+++ vue-admin-artiely/src/common/utils/index.js
@@ -7,6 +7,12 @@
 export function GetQueryString (name) {
   var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)')
   var r = window.location.search.substr(1).match(reg)
+  var s = window.location.search
+  var href = window.location.href
+  var index = href.indexOf('?')
+  if (s === '') {
+    r = href.substr(index + 1).match(reg)
+  }
   if (r != null) return unescape(r[2])
   return null
 }
